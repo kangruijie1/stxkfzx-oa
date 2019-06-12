bb7af06322d091458ea0533fba762356c22d63a6
krjaydog <1016114277@qq.com>
2019-06-12 17:43:11 +0800

    use dubbo

---
+++ kfzxoa-auth/src/main/java/xyz/stxkfzx/manager/auth/KfzxoaAuthApplication.java
@@ -0,0 +1,13 @@
+package xyz.stxkfzx.manager.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfzxoaAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfzxoaAuthApplication.class, args);
+    }
+
+}
---
+++ kfzxoa-common/src/main/java/xyz/stxkfzx/manager/common/KfzxoaCommonApplication.java
@@ -0,0 +1,13 @@
+package xyz.stxkfzx.manager.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KfzxoaCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KfzxoaCommonApplication.class, args);
+    }
+
+}
